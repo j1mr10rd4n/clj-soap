7e66ff0f68bcd3d89a9b74ce93339ff89ec87a80
Jim Riordan <jim@j1mr10rd4n.info>
2017-03-16 19:33:04 +1000

    bump axis version to 1.6.4

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.apache.axis2/axis2-adb "1.6.3"]
-                 [org.apache.axis2/axis2-transport-http "1.6.3"]
-                 [org.apache.axis2/axis2-transport-local "1.6.3"]]
+                 [org.apache.axis2/axis2-adb "1.6.4"]
+                 [org.apache.axis2/axis2-transport-http "1.6.4"]
+                 [org.apache.axis2/axis2-transport-local "1.6.4"]]
   :source-paths ["src" "test"]
   :aot [clj-soap.test.core])
