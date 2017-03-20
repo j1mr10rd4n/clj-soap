e2ef7e2d7561caadef7d97dd64f0d06554b50eb4
Jim Riordan <jim@j1mr10rd4n.info>
2017-03-20 14:34:19 +1000

    use clojure 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/j1mr10rd4n/clj-soap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.axis2/axis2-adb "1.7.4"]
                  [org.apache.axis2/axis2-transport-http "1.7.4"]
                  [org.apache.axis2/axis2-transport-local "1.7.4"]
